24419c6cb5157fd18cc13d329a25f66eae1c90a6
Steven Wu <steven@cyber-blog.com>
2014-06-24 12:43:41 +0100

    Adding shortcode

--- functions.php
+++ functions.php
@@ -29,4 +29,32 @@ require_once('library/enqueue-scripts.php');
 // Add theme support
 require_once('library/theme-support.php');
 
+// Shortcodes
+function foundation_add_alerts( $atts, $content = null ) {
+	extract( shortcode_atts( array( 
+		'type'  => '',     
+		'shape' => '',     
+		'close' => 'true', 
+		'class' => ''
+	 ), $atts ) );
+	 
+	$class_array[] = ( $shape ) ? $shape : '';
+	$class_array[] = ( $type ) ? $type : '';
+	$class_array[] = ( $class ) ? $class : '';
+	$class_array = array_filter( $class_array );
+	$classes = implode( ' ', $class_array );
+	 
+	$output  = '<div class="alert-box ' . $classes . '">';
+	$output .= do_shortcode( $content );
+	$output .= ( 'false' != $close ) ? '<a class="close" href="">&times;</a>' : '';
+	$output .= '</div>';
+		
+	return $output;
+}
+
+function register_shortcodes() {
+   add_shortcode('alert', 'foundation_add_alerts');
+}
+add_action('init', 'register_shortcodes');
+
 ?>
